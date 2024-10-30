c09a313a0ce907d2e33128ebb599bfa298995730
Víctor Candido <victorev@outlook.com>
2024-10-29 22:03:38 -0300

    Setting up shadcn ui

--- app/home.spec.tsx
+++ app/home.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Home from './page';
 
 describe('Home', () => {
-    it('should render successfully', () => {
+    it('should have a header', () => {
         render(<Home />);
 
         const headerElement = screen.getByRole('heading', {
@@ -11,4 +11,15 @@ describe('Home', () => {
 
         expect(headerElement).toBeTruthy();
     });
+
+    it('should have a button with "Click me" label', () => {
+        render(<Home />);
+
+        const buttonElement = screen.getByRole('button', {
+            name: 'Click me'
+        });
+
+        expect(buttonElement).toBeTruthy();
+    });
+
 })
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1>Victor Candido Portfolio</h1>
+    <div>
+      <h1>Victor Candido Portfolio</h1>
+
+      <Button>
+        Click me
+      </Button>
+    </div>
   );
 }
